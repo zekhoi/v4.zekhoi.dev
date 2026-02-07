064ea8acdead9bebc1b55818ff745c9d507ab669
Khoironi Kurnia Syah <khoironidev@gmail.com>
2026-02-07 20:57:43 +0800

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,8 +54,30 @@ export const metadata: Metadata = {
   creator: 'Khoironi Kurnia Syah',
   publisher: 'Khoironi Kurnia Syah',
   icons: {
-    icon: '/favicon.ico'
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/android-chrome-192x192.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '512x512',
+        url: '/android-chrome-512x512.png'
+      }
+    ]
   },
+  manifest: '/site.webmanifest',
   openGraph: {
     type: 'website',
     locale: 'en_US',
